69ae23961bd70dfab4bf36497a58c72c426a57ae
Uros Mitrovic <umitrovic22@gmail.com>
2023-01-22 22:33:41 +0100

    Added exit button functionality and coded placeholders for fields on Log In form

--- AppLotProject/PresentationLayer/LogInForm.Designer.cs
+++ AppLotProject/PresentationLayer/LogInForm.Designer.cs
@@ -30,8 +30,8 @@
         {
             System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(LogInForm));
             this.label1 = new System.Windows.Forms.Label();
-            this.textBox1 = new System.Windows.Forms.TextBox();
-            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.textBoxUsername = new System.Windows.Forms.TextBox();
+            this.textBoxPassword = new System.Windows.Forms.TextBox();
             this.panel1 = new System.Windows.Forms.Panel();
             this.button2 = new System.Windows.Forms.Button();
             this.btnLogIn = new System.Windows.Forms.Button();
@@ -51,28 +51,32 @@
             this.label1.TabIndex = 0;
             this.label1.Text = "Welcome to Car Lot app";
             // 
-            // textBox1
+            // textBoxUsername
             // 
-            this.textBox1.BackColor = System.Drawing.Color.OrangeRed;
-            this.textBox1.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
-            this.textBox1.Font = new System.Drawing.Font("Segoe UI", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.textBox1.ForeColor = System.Drawing.Color.Snow;
-            this.textBox1.Location = new System.Drawing.Point(39, 55);
-            this.textBox1.Name = "textBox1";
-            this.textBox1.Size = new System.Drawing.Size(222, 27);
-            this.textBox1.TabIndex = 1;
+            this.textBoxUsername.BackColor = System.Drawing.Color.OrangeRed;
+            this.textBoxUsername.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.textBoxUsername.Font = new System.Drawing.Font("Segoe UI Semilight", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textBoxUsername.ForeColor = System.Drawing.Color.DimGray;
+            this.textBoxUsername.Location = new System.Drawing.Point(39, 55);
+            this.textBoxUsername.Name = "textBoxUsername";
+            this.textBoxUsername.Size = new System.Drawing.Size(222, 27);
+            this.textBoxUsername.TabIndex = 1;
+            this.textBoxUsername.Text = "Enter your username:";
+            this.textBoxUsername.MouseClick += new System.Windows.Forms.MouseEventHandler(this.textBoxUsername_MouseClick);
             // 
-            // textBox2
+            // textBoxPassword
             // 
-            this.textBox2.BackColor = System.Drawing.Color.OrangeRed;
-            this.textBox2.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
-            this.textBox2.Font = new System.Drawing.Font("Segoe UI", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.textBox2.ForeColor = System.Drawing.Color.Snow;
-            this.textBox2.Location = new System.Drawing.Point(39, 93);
-            this.textBox2.Name = "textBox2";
-            this.textBox2.PasswordChar = '*';
-            this.textBox2.Size = new System.Drawing.Size(222, 27);
-            this.textBox2.TabIndex = 2;
+            this.textBoxPassword.BackColor = System.Drawing.Color.OrangeRed;
+            this.textBoxPassword.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.textBoxPassword.Font = new System.Drawing.Font("Segoe UI", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textBoxPassword.ForeColor = System.Drawing.Color.DimGray;
+            this.textBoxPassword.Location = new System.Drawing.Point(39, 93);
+            this.textBoxPassword.Name = "textBoxPassword";
+            this.textBoxPassword.PasswordChar = '*';
+            this.textBoxPassword.Size = new System.Drawing.Size(222, 27);
+            this.textBoxPassword.TabIndex = 2;
+            this.textBoxPassword.Text = "Enter the password";
+            this.textBoxPassword.MouseClick += new System.Windows.Forms.MouseEventHandler(this.textBoxPassword_MouseClick);
             // 
             // panel1
             // 
@@ -80,8 +84,8 @@
             this.panel1.BackgroundImageLayout = System.Windows.Forms.ImageLayout.None;
             this.panel1.Controls.Add(this.button2);
             this.panel1.Controls.Add(this.btnLogIn);
-            this.panel1.Controls.Add(this.textBox1);
-            this.panel1.Controls.Add(this.textBox2);
+            this.panel1.Controls.Add(this.textBoxUsername);
+            this.panel1.Controls.Add(this.textBoxPassword);
             this.panel1.Location = new System.Drawing.Point(74, 72);
             this.panel1.Name = "panel1";
             this.panel1.Size = new System.Drawing.Size(312, 217);
@@ -120,6 +124,9 @@
             this.btnX.Size = new System.Drawing.Size(13, 13);
             this.btnX.TabIndex = 4;
             this.btnX.Text = "X";
+            this.btnX.MouseClick += new System.Windows.Forms.MouseEventHandler(this.btnX_MouseClick);
+            this.btnX.MouseEnter += new System.EventHandler(this.btnX_MouseEnter);
+            this.btnX.MouseLeave += new System.EventHandler(this.btnX_MouseLeave);
             // 
             // LogInForm
             // 
@@ -149,8 +156,8 @@
         #endregion
 
         private System.Windows.Forms.Label label1;
-        private System.Windows.Forms.TextBox textBox1;
-        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.TextBox textBoxUsername;
+        private System.Windows.Forms.TextBox textBoxPassword;
         private System.Windows.Forms.Panel panel1;
         private System.Windows.Forms.Button button2;
         private System.Windows.Forms.Button btnLogIn;
--- AppLotProject/PresentationLayer/LogInForm.cs
+++ AppLotProject/PresentationLayer/LogInForm.cs
@@ -15,6 +15,37 @@ namespace PresentationLayer
         public LogInForm()
         {
             InitializeComponent();
+            
+        }
+
+        private void btnX_MouseClick(object sender, MouseEventArgs e)
+        {
+            this.Close();
+        }
+
+       
+
+        private void btnX_MouseEnter(object sender, EventArgs e)
+        {
+            btnX.ForeColor = Color.OrangeRed;
+        }
+
+        private void btnX_MouseLeave(object sender, EventArgs e)
+        {
+            btnX.ForeColor = Color.Black;
+        }
+
+        private void textBoxPassword_MouseClick(object sender, MouseEventArgs e)
+        {
+            textBoxPassword.Text = "";
+            textBoxPassword.ForeColor = Color.Snow;
+
+        }
+
+        private void textBoxUsername_MouseClick(object sender, MouseEventArgs e)
+        {
+            textBoxUsername.Text = "";
+            textBoxUsername.ForeColor = Color.Snow;
         }
     }
 }
