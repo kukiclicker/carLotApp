d12a5bd9b23f6dfd67048c34105826d61b9e0636
Uros Mitrovic <umitrovic22@gmail.com>
2023-01-30 21:55:00 +0100

    Basic design and close functionality for register form

--- AppLotProject/PresentationLayer/Registration.Designer.cs
+++ AppLotProject/PresentationLayer/Registration.Designer.cs
@@ -28,12 +28,43 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Registration));
+            this.btnRegisterX = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // btnRegisterX
+            // 
+            this.btnRegisterX.AutoSize = true;
+            this.btnRegisterX.BackColor = System.Drawing.Color.Transparent;
+            this.btnRegisterX.ForeColor = System.Drawing.Color.Snow;
+            this.btnRegisterX.Location = new System.Drawing.Point(786, -1);
+            this.btnRegisterX.Name = "btnRegisterX";
+            this.btnRegisterX.Size = new System.Drawing.Size(14, 13);
+            this.btnRegisterX.TabIndex = 0;
+            this.btnRegisterX.Text = "X";
+            this.btnRegisterX.MouseClick += new System.Windows.Forms.MouseEventHandler(this.btnRegisterX_MouseClick);
+            this.btnRegisterX.MouseEnter += new System.EventHandler(this.btnRegisterX_MouseEnter);
+            this.btnRegisterX.MouseLeave += new System.EventHandler(this.btnRegisterX_MouseLeave);
+            // 
+            // Registration
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackgroundImage = global::PresentationLayer.Properties.Resources.background_img1;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ControlBox = false;
+            this.Controls.Add(this.btnRegisterX);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "Registration";
             this.Text = "Registration";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Label btnRegisterX;
     }
 }
--- AppLotProject/PresentationLayer/Registration.cs
+++ AppLotProject/PresentationLayer/Registration.cs
@@ -16,5 +16,20 @@ namespace PresentationLayer
         {
             InitializeComponent();
         }
+
+        private void btnRegisterX_MouseClick(object sender, MouseEventArgs e)
+        {
+            this.Close();
+        }
+
+        private void btnRegisterX_MouseEnter(object sender, EventArgs e)
+        {
+            btnRegisterX.ForeColor= Color.OrangeRed;
+        }
+
+        private void btnRegisterX_MouseLeave(object sender, EventArgs e)
+        {
+            btnRegisterX.ForeColor = Color.Snow;
+        }
     }
 }
